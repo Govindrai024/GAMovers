551b02283eff797aee96859b22ab8ed0c1af681c
affandahar <daharaffan@gmail.com>
2020-12-23 22:15:00 +0500

    tab navigation added

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import StackNavigator from './navigation/AuthStack/'
 import {  Provider as PaperProvider } from 'react-native-paper';
 import  AppLoading  from 'expo-app-loading';
 import { useFonts } from 'expo-font';
+
+
+import MyStack from './navigation/AuthStack'
 
 //Screns
 import Home from './screens/home/home'
@@ -22,103 +25,6 @@ import ContactUs from "./screens/contactUs/ContactUS";
 //Utilities                                                                  
 import theme from './utilities/theme';
 
-const Stack = createStackNavigator();
-
-const StackNavigator = () => {
-  return(
-      <Stack.Navigator
-        screenOptions={{
-          headerTitleAlign: 'center',
-          headerStyle: {
-            backgroundColor: '#56ab2f',
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold',
-          },
-        }}
-
-      >
-        <Stack.Screen
-          name="Welcome"
-          component={Welcome}
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="Login"
-          component={Login}
-          options={{
-            headerShown: false,
-          }}
-        /> 
-        <Stack.Screen
-          name="Signup"
-          component={Signup}
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{
-            headerShown: true,
-
-          }}
-        />
-
-        <Stack.Screen
-          name="BookingForm"
-          component={BookingForm}
-          options={{
-            headerShown: true,
-
-          }}
-        />
-         <Stack.Screen
-          name="BookingForm2"
-          component={BookingForm2}
-          options={{
-            headerShown: true,
-
-          }}
-        />
-
-        <Stack.Screen
-          name="Profile"
-          component={Profile}
-          options={{
-            headerShown: true,
-
-          }}
-        /> 
-        
-        {/* <Stack.Screen
-          name="UserProfile"
-          component={UserProfile}
-          options={{
-            headerShown: true,
-
-          }}
-        /> */}
-
-         <Stack.Screen
-          name="ContactUs"
-          component={ContactUs}
-          options={{
-            headerShown: true,
-
-          }}
-        /> 
-
-
-      </Stack.Navigator>
-  )
-}
-
-
 function App(props) {
   let [fontsLoaded] = useFonts({
     'Nunito-Bold': require('./assets/fonts/Nunito-Bold.ttf'),
@@ -130,7 +36,8 @@ function App(props) {
     return (
       <NavigationContainer>
         <PaperProvider theme={theme}>
-        <StackNavigator/>
+        <StackNavigator/> 
+
         </PaperProvider>
         
       </NavigationContainer>
--- screens/bookingForm/bookingForm.js
+++ screens/bookingForm/bookingForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {  Text, View,TouchableWithoutFeedback,ScrollView, Button } from 'react-native';
+import {  Text, View,TouchableWithoutFeedback,ScrollView, Button , Keyboard} from 'react-native';
 import { TextInput } from "react-native-paper";
 
 
